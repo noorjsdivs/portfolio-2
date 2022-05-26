77fe5201011a0c315c0abfcd6fcab676c02c92fd
Noor_Mohammad <noormohammad@aaoman.com.om>
2022-05-26 15:44:55 +0400

    updated with the theme toggler

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -18,7 +18,31 @@ function setActiveStyle(color) {
     if (color === style.getAttribute("title")) {
       style.removeAttribute("disabled");
     } else {
-      style.Attribute("disabled", "true");
+      style.setAttribute("disabled", "true");
     }
   });
 }
+// setActiveStyle();
+
+// Light and Dark Theme
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () => {
+  dayNight.querySelector("i").classList.toggle("fa-sun");
+  dayNight.querySelector("i").classList.toggle("fa-moon");
+  document.body.classList.toggle("dark");
+});
+window.addEventListener("load", () => {
+  if (document.body.classList.contains("dark")) {
+    dayNight.querySelector("i").classList.add("fa-sun");
+  } else {
+    dayNight.querySelector("i").classList.add("fa-moon");
+  }
+});
+
+// =============active state for nav
+// const nav = document.querySelector(".nav");
+// const navActive = document.querySelector(".navactive");
+// console.log(nav);
+// navActive.addEventListener("click", () => {
+//   style.removeAttribute("navactive");
+// });
